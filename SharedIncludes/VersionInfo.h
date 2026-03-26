7ef1d219c810f9e1f9325da9070f157128f6deb4
zhongfly <11155705+zhongfly@users.noreply.github.com>
2026-03-27 00:07:14 +0800

    add version info to build outputs

---
+++ SharedIncludes/VersionInfo.h
@@ -0,0 +1,15 @@
+#ifndef SFH_VERSION_INFO_H
+#define SFH_VERSION_INFO_H
+
+#define SFH_VERSION_MAJOR 1
+#define SFH_VERSION_MINOR 1
+#define SFH_VERSION_PATCH 0
+#define SFH_VERSION_BUILD 0
+
+#define SFH_VERSION_NUMERIC 1,1,0,0
+#define SFH_VERSION_STRING "1.1.0"
+#define SFH_COMPANY_NAME "SubtitleFontHelper"
+#define SFH_PRODUCT_NAME "SubtitleFontHelper"
+#define SFH_LEGAL_COPYRIGHT "Copyright (C) SubtitleFontHelper"
+
+#endif

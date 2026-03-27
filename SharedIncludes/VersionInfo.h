05337bc575db8a7246af011ed7482ccab8af2be7
zhongfly <11155705+zhongfly@users.noreply.github.com>
2026-03-27 23:19:43 +0800

    bump version to 1.2.0

--- SharedIncludes/VersionInfo.h
+++ SharedIncludes/VersionInfo.h
@@ -2,12 +2,12 @@
 #define SFH_VERSION_INFO_H
 
 #define SFH_VERSION_MAJOR 1
-#define SFH_VERSION_MINOR 1
+#define SFH_VERSION_MINOR 2
 #define SFH_VERSION_PATCH 0
 #define SFH_VERSION_BUILD 0
 
-#define SFH_VERSION_NUMERIC 1,1,0,0
-#define SFH_VERSION_STRING "1.1.0"
+#define SFH_VERSION_NUMERIC 1,2,0,0
+#define SFH_VERSION_STRING "1.2.0"
 #define SFH_COMPANY_NAME "SubtitleFontHelper"
 #define SFH_PRODUCT_NAME "SubtitleFontHelper"
 #define SFH_LEGAL_COPYRIGHT "Copyright (C) SubtitleFontHelper"

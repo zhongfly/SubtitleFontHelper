7ce008db6a669a1c8e924a59173697b4a13c735f
zhongfly <11155705+zhongfly@users.noreply.github.com>
2026-04-05 17:02:17 +0800

    chore: bump version to v1.3.0
    Update resource version metadata and package manifest version to 1.3.0.

--- SharedIncludes/VersionInfo.h
+++ SharedIncludes/VersionInfo.h
@@ -2,12 +2,12 @@
 #define SFH_VERSION_INFO_H
 
 #define SFH_VERSION_MAJOR 1
-#define SFH_VERSION_MINOR 2
-#define SFH_VERSION_PATCH 5
+#define SFH_VERSION_MINOR 3
+#define SFH_VERSION_PATCH 0
 #define SFH_VERSION_BUILD 0
 
-#define SFH_VERSION_NUMERIC 1,2,5,0
-#define SFH_VERSION_STRING "1.2.5"
+#define SFH_VERSION_NUMERIC 1,3,0,0
+#define SFH_VERSION_STRING "1.3.0"
 #define SFH_COMPANY_NAME "SubtitleFontHelper"
 #define SFH_PRODUCT_NAME "SubtitleFontHelper"
 #define SFH_LEGAL_COPYRIGHT "Copyright (C) SubtitleFontHelper"
